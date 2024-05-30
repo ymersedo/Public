d098bc6e44f62373144183e0850ec2fe11a63075
ymersedo <cedricslayer@gmail.com>
2024-05-30 10:54:14 +0800

    Add files via upload

---
+++ Practice.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Practice {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        // Lines of code inside the try block will be checked for exceptions
+        try {
+            System.out.println("Please enter a number: ");
+            int num = scanner.nextInt();
+
+            System.out.println("The number you've entered is: " + num);
+        }
+        // This will catch any exceptions
+        catch(Exception e) {
+            System.out.println("Error running code!");
+        }
+        // This will always execute
+        finally {
+            scanner.close();
+            System.out.println("Scanner closed!");
+        }
+    }
+}
